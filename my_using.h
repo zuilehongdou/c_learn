f5c2ebbd80f755614d855dcb9bbc7fbefe00ff4d
zuilehongdou <zuilehongdou@gmail.com>
2016-02-23 14:29:05 +0800

    add using file

---
+++ my_using.h
@@ -0,0 +1,11 @@
+#ifndef MY_USING_H
+#define MY_USING-H
+#include<iostream>
+#include<string>
+#include<vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+#endif
