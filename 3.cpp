0f714c776b51951f9d30e61f4857a8c5d9bc3263
zuilehongdou <zuilehongdou@gmail.com>
2016-02-23 15:43:15 +0800

    add 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,22 @@
+//多维数组的使用
+#include "my_using.h"
+int main()
+{
+    int arr[3][3]={0};
+    for(size_t i=0;i<3;i++)
+    {
+        for(size_t j=0;j<3;j++)
+        {
+            arr[i][j]=i*3+j;
+        }
+    }
+    for(auto &row:arr)
+    {
+        for(auto &col:row)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
