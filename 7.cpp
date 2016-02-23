0e8fbbc80426b9204fd418ef2546b582c4adef80
zuilehongdou <zuilehongdou@gmail.com>
2016-02-23 20:06:33 +0800

    move to demo

--- 7.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    for(string str;getline(cin,str);cout<<str<<endl);
-    return 0;
-}
--- 8.cpp
+++
@@ -1,8 +0,0 @@
-//输入单词
-#include <iostream>
-using namespace std;
-int main()
-{
-    for(string str;cin>>str;cout<<str<<endl);
-    return 0;
-}
