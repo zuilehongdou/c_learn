259d676b43e40814ca45f4d0346095c29083cef4
zuilehongdou <zuilehongdou@gmail.com>
2016-02-24 14:47:53 +0800

    add black

--- demo/12.cpp
+++ demo/12.cpp
@@ -4,9 +4,9 @@ int main()
 {
     int i;
     double d;
-    d=i=3.5;
-    cout<<d<<i<<endl;
-    i=d=3.5;
-    cout<<d<<i<<endl;
+    d = i = 3.5;
+    cout<< d << i <<endl;
+    i = d = 3.5;
+    cout<< d << i <<endl;
     return 0;
 }
